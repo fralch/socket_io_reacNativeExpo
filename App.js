b65db1a2896cc39bc03c9406c9361e6a70bae12f
Frank Cairampoma <ingfralch@gmail.com>
2023-01-18 18:01:47 -0500

    usando nueva configuracion

--- App.js
+++ App.js
@@ -1,53 +1,37 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import io from "socket.io-client";
 
-// Replace this URL with your own socket-io host, or start the backend locally
-const socketEndpoint = "http://127.0.0.1:3000";
-
 export default function App() {
-  const [hasConnection, setConnection] = useState(false);
-  const [time, setTime] = useState(null);
 
-  useEffect(function didMount() {
-    const socket = io(socketEndpoint, {
-      transports: ["websocket"],
-    });
+  const [socket, setSocket] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [message, setMessage] = useState("");
 
-    socket.io.on("open", () => setConnection(true));
-    socket.io.on("close", () => setConnection(false));
-
-    socket.on("time-msg", (data) => {
-      setTime(new Date(data.time).toString());
-    });
-
-    return function didUnmount() {
-      socket.disconnect();
-      socket.removeAllListeners();
-    };
+  useEffect(() => {
+    const socket = io("http://127.0.0.1:3000");
+    setSocket(socket);
+    setLoading(false);
   }, []);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("chatMessage", (msg) => {
+        setMessage(msg);
+      });
+    }
+  }, [socket]);
+
+  if (loading) {
+    return (
+      <ActivityIndicator size="large" color="#0000ff" />
+    );
+  }
 
   return (
     <View style={styles.container}>
-      {!hasConnection && (
-        <>
-          <Text style={styles.paragraph}>
-            Connecting to {socketEndpoint}...
-          </Text>
-          <Text style={styles.footnote}>
-            Make sure the backend is started and reachable :/
-          </Text>
-        </>
-      )}
-
-      {hasConnection && (
-        <>
-          <Text style={[styles.paragraph, { fontWeight: "bold" }]}>
-            Server time
-          </Text>
-          <Text style={styles.paragraph}>{time}</Text>
-        </>
-      )}
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{message.msg}</Text>
     </View>
   );
 }
@@ -58,12 +42,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  paragraph: {
-    fontSize: 16,
-  },
-  footnote: {
-    fontSize: 14,
-    fontStyle: "italic",
   },
 });
